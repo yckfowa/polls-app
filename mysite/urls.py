9c6bba1f3600bdcbe5b7ad7f27a4e0fab3c273df
yckfowa <80372122+yckfowa@users.noreply.github.com>
2021-06-26 20:30:58 +0800

    Create home, register & login page

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from polls import views
 from django.urls import include, path
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.loginPage, name='loginPage'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
